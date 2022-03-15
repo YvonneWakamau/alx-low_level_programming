b418c0adaf002bbcb4a4c5680c27ccf1a432b609
yvonnewakamau <yvonnefaith58@gmail.com>
2022-03-15 03:25:40 -0700

    zero

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,2 +1,13 @@
-_putchar.c
-+ *main.c
+@@ -1,9 +1,9 @@
+#include "_putchar.h"
+/**
+  - * main - ... 
+ + * main - Entry point
+   *
+ - * Return: Always (Success)
+  + * Return: Always 0
+          
+   */
+int main(void)
+
+{
