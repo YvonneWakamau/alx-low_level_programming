4f6bf3aa4020c8b0dcc30057df4981c2b6a5759d
yvonnewakamau <yvonnefaith58@gmail.com>
2022-03-15 04:48:50 -0700

    zero

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,13 +1,24 @@
-@@ -1,9 +1,9 @@
-#include "_putchar.h"
+#include <unistd.h>
+#include "main.h"
+
 /**
-  - * main - ... 
- + * main - Entry point
-   *
- - * Return: Always (Success)
-  + * Return: Always 0
-          
-   */
+  * main - putchar 
+  *
+  * _putchar print the character
+  * Return: Always 0 (Success)      
+  */
 int main(void)
 
 {
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0)
+}		
+
