198a4220a1deffdf14d230d1abac0b2c7f27b8f3
yvonnewakamau <yvonnefaith58@gmail.com>
2022-03-15 10:30:11 -0700

    putC

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -2,11 +2,11 @@
 
 /**
   * main - prints _putchar
-   *
-    * Description: prints _putchar
-     *
-      * Return: 0 (Success)
-        */
+  *
+  * Description: prints _putchar
+  *
+  * Return: 0 (Success)
+  */
 int main(void)
 {
 char text[9] = "_putchar";
