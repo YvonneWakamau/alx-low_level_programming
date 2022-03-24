9c5a254186f5c155d6fbadee440aa8e1c770c492
yvonnewakamau <yvonnefaith58@gmail.com>
2022-03-24 12:02:26 -0700

    103

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -8,7 +8,6 @@
  * @r: buffer that the function will use to store the result.
  * @size_r: buffer size:
  * Return: the pointer to dest.
- *
  */
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
