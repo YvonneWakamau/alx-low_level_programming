ded4f9bbae6ccef6625e864c50b8fa8ac9faa0ce
yvonnewakamau <yvonnefaith58@gmail.com>
2022-03-28 03:48:50 -0700

    1

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -2,19 +2,18 @@
 
 /**
  * _memcpy - copy memory area
- * @dest: dest memory area
- * @src: source memory area
+ *@dest: dest memory area
+ *@src: source memory area
  * @n: bytes from memory area to copy
  *
  * Return: memory area replaced
  */
-int _putchar(char c);
-char *_memcpy(char *dest, char *src, unsigned int n);
+char *_memcpy(char *dest, char *src, unsigned int n)
 {
-  unsigned int a;
-  for (a = 0; a < n; a++)
+unsigned int a;
+for (a = 0; a < n; a++)
 {
-  dest[a] = src[a];
+dest[a] = src[a];
 }
-  return (dest);
+return (dest);
 }
