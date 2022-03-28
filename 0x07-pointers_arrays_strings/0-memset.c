c9a6a2f5622c6351e591d1265c9161af51d12bca
yvonnewakamau <yvonnefaith58@gmail.com>
2022-03-28 03:22:57 -0700

    zero

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -9,7 +9,7 @@
  * Return: the memory area filled
  */
 int _putchar(char c);
-char *_memset(char *s, char b, unsigned int n)
+char *_memset(char *s, char b, unsigned int n);
 {
 unsigned int a;
 for (a = 0; a < n; a++)
