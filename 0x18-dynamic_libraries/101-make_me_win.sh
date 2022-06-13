5920c7850bc598eb6d6ca8ddd0a6376f1ada9d6b
YvonneWakamau <yvonnefaith58@gmail.com>
2022-06-13 21:33:43 +0300

    dsf

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp https://github.com/YvonneWakamau/alx-low_level_programming/tree/main/0x18-dynamic_libraries/libmask.so
+wget -P /tmp .github/0x18-dynamic_libraries/libmask.so
 export LD_PRELOAD=/tmp/libmask.so
